e00a6479cc52f8278b711cb50330fc6c6aa02555
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-06-06 10:37:59 -0300

    Firebase configurado via linha de comando  - flutterfire configure Aplicativo Assinado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:job_time/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -11,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Job Timer',
       theme: ThemeData(
         // This is the theme of your application.
         //
