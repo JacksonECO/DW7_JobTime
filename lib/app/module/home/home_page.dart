006b6d88d6d177acfc9c68277a0d00e7d9d44d59
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-06-07 23:18:38 -0300

    Estrutura inicial da home

--- lib/app/module/home/home_page.dart
+++ lib/app/module/home/home_page.dart
@@ -1,31 +1,84 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:job_time/app/core/database/database.dart';
-import 'package:job_time/app/entities/project.dart';
-import 'package:job_time/app/entities/project_status.dart';
+import 'package:job_time/app/module/home/widgets/header_projects_menu.dart';
+import 'package:job_time/app/services/auth/auth_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          ElevatedButton(
-              onPressed: () async {
-                final db = Modular.get<Database>();
-                final connection = await db.openConnection();
-                connection.writeTxn((isar) {
-                  var project = Project();
-                  project.name = 'Teste';
-                  project.status = ProjectStatus.finalizado;
-
-                  return connection.projects.put(project);
-                });
-              },
-              child: const Text('Cadastrar')),
-        ],
+    return Scaffold(
+      drawer: Drawer(
+        child: SafeArea(
+          child: ListTile(title: const Text('Sair'), onTap: () => Modular.get<AuthService>().signOut()),
+        ),
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            const SliverAppBar(
+              title: Text('Projetos'),
+              expandedHeight: 100,
+              toolbarHeight: 100,
+              centerTitle: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(15),
+                ),
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: HeaderProjectsMenu(),
+              pinned: true,
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate([
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                  child: const Center(child: Text('FIM')),
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: 200,
+                ),
+              ]),
+            ),
+          ],
+        ),
       ),
     );
   }
