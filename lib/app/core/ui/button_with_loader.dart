ef065619c1130f9c1bacda23e76368750b1a85a0
Jackson Galdino <jacksongdas.jackson@gmail.com>
2022-06-16 11:06:45 -0300

    Modificação do button_with_loader; Simplificação

--- lib/app/core/ui/button_with_loader.dart
+++ lib/app/core/ui/button_with_loader.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ButtonWithLoader<B extends StateStreamable<S>, S> extends ElevatedButton {
-  final BlocWidgetSelector<S, bool> selector;
+  final BlocWidgetSelector<S, bool>? selector;
+  final S? selectorStatusLoading;
   final B block;
   final String label;
 
   ButtonWithLoader({
-    required this.selector,
+    this.selector,
+    this.selectorStatusLoading,
     required this.block,
     required this.label,
     required super.onPressed,
     super.key,
-  }) : super(
+  })  : assert(selector != null || selectorStatusLoading != null),
+        super(
           child: BlocSelector<B, S, bool>(
             bloc: block,
-            selector: selector,
+            selector: selector ?? (state) => state == selectorStatusLoading!,
             builder: (context, loading) {
               if (!loading) {
                 return Text(label);
